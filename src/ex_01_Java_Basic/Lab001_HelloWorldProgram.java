33468ca0af81e077dd774eed5e51962816f6cd9a
navip <nswamyg62@gmail.com>
2025-12-30 05:54:40 +0530

    2nd lab program added. this is the first commit during lab2.

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -1,4 +1,7 @@
 package ex_01_Java_Basic;
 
 public class Lab001_HelloWorldProgram {
+    static void main() {
+        System.out.println("welcome to JAVA PROGRAMMING");
+    }
 }
---
+++ src/ex_01_Java_Basic/Lab002_HelloWorldProgram.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab002_HelloWorldProgram {
+    static void main() {
+        System.out.println("welcome to JAVA PROGRAMMING-2");
+    }
+}
