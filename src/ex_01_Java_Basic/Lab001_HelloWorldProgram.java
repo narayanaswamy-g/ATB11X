8d65bbaa873136a7f8fb18f06722ab88fb20a9d5
navip <nswamyg62@gmail.com>
2026-01-05 07:21:08 +0530

    2nd lab program added. this is the first commit during lab2.

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -1,7 +1,11 @@
 package ex_01_Java_Basic;
-
+//Author-Naveen
 public class Lab001_HelloWorldProgram {
     static void main() {
         System.out.println("welcome to JAVA PROGRAMMING");
+        //my first example program
     }
 }
+/*ATB learning in progress
+multiple comments check. Intellisense auto recognizes the multiline comments
+ */
