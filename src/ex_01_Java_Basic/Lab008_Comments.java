68723e83bd8b10d49ccccdcbd466b3fc8e136ee7
navip <nswamyg62@gmail.com>
2026-01-05 07:41:45 +0530

    2nd lab program added. this is the first commit during lab2.

---
+++ src/ex_01_Java_Basic/Lab008_Comments.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+//Author-Naveen
+public class Lab008_Comments {
+    static void main() {
+        System.out.println("welcome to learn Comments");
+        //double slash for single line comment
+        // "/*" opening and "*/" closing for multiline commenting
+        // select a block of code and use command "//" for commenting a block
+        }
+}
+/*ATB learning in progress
+multiple comments check. Intellisense auto recognizes the multiline comments
+ */
