02c7c4ed4db7552d70688b5dbf9b82546114f812
navip <nswamyg62@gmail.com>
2026-01-05 08:08:18 +0530

    Keywords and identifies program added

---
+++ src/ex_01_Java_Basic/LAB009_Keywords_Identifiers.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class LAB009_Keywords_Identifiers {
+    static void main(){
+        System.out.println("keywords are reserved words");
+        System.out.println("Identifiers are named variables");
+    }
+}
