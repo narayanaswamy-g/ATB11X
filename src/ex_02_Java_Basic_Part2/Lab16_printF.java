f0d7dbae65478c949725021f83ebded0950e8685
navip <nswamyg62@gmail.com>
2026-01-07 06:13:57 +0530

    learn variabless

---
+++ src/ex_02_Java_Basic_Part2/Lab16_printF.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basic_Part2;
+
+public class Lab16_printF {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.printf("Integer value of a is %d%n", a);
+        System.out.printf("Float value of a is %.2f%n", (float) a);
+        System.out.printf("Boolean value of a is %b%n", a);
+    }
+}
