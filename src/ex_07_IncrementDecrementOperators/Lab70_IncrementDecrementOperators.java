6cf2461520020d9ae2dffda1d1613e46cca35318
navip <nswamyg62@gmail.com>
2026-01-09 03:42:55 +0530

    learn Increment - Decrement operator

---
+++ src/ex_07_IncrementDecrementOperators/Lab70_IncrementDecrementOperators.java
@@ -0,0 +1,14 @@
+package ex_07_IncrementDecrementOperators;
+
+public class Lab70_IncrementDecrementOperators {
+    static void main() {
+        int a=10;
+        //PRE-INCREMENT
+        System.out.println(++a);
+        System.out.println(a);
+        //POST INCREMENT
+        int b=20;
+        System.out.println(b++);
+        System.out.println(b);
+    }
+}
